6e5d86508935464f1e65ad93a9aade9444b7425a
John <johnsief12@gmail.com>
2024-01-26 18:33:48 -0500

    traversing the dom of the 3 questions

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+const questionBtn = document.querySelectorAll('.question-btn');
+
+questionBtn.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  });
+});
